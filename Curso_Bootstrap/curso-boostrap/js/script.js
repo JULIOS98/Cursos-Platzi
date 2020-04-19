fbe96ebdfcd338aefad459e6cc24f94969f603f0
Julio Gonzalez <1630368@upv.edu.mx>
2020-04-19 00:29:45 -0500

    Primer Commit de cursos de platzi

---
+++ Curso_Bootstrap/curso-boostrap/js/script.js
@@ -0,0 +1,11 @@
+let persona 
+[
+    firstName = " maria ",
+    lastName = " valeria",
+    Id = 173738
+];
+let dato = persona.firstName + " " + persona. lastName ;
+console.log(dato);
+
+
+
---
+++ Curso_Html_Css/curso_html_css/js/script.js
@@ -0,0 +1,5 @@
+let a = 1;
+let b = 5;
+let cadena = "hola";
+
+alert(cadena);
