178a06ea43723ce6febd43cfc56c7c4eedaa2746
Julio Gonzalez <1630368@upv.edu.mx>
2020-05-14 00:13:56 -0500

    API con TDD: Store

--- Curso_Introduccion_Laravel/Proyecto_API_TDD/tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ Curso_Introduccion_Laravel/Proyecto_API_TDD/tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -8,15 +8,21 @@ use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_store()
+    {
+        //$this->withoutExceptionHandling();
+        $response = $this->json('POST','/api/posts',[
+            'title' => 'El post de prueba',
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'El post de prueba'])
+            ->assertStatus(201); //OK, creado un recurso
+
+        $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
+
+     
     }
 }
