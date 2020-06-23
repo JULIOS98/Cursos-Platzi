f9114f96ad2c110015aff8bd3ea10449a2f3b372
Julio Gonzalez <1630368@upv.edu.mx>
2020-06-23 01:20:23 -0500

    Se agrego el curso de js basico

---
+++ Curso_Basico_Javascript/js/funciones.js
@@ -0,0 +1,18 @@
+//Funciones declarativas
+
+function miFuncion()
+{
+    return 3;
+}
+
+miFuncion();
+
+
+//Funciones Expresivas o anonimas
+let miFuncion = function(a,b){
+
+    return a + b;
+}
+    
+
+miFuncion(5,5);
---
+++ Curso_Basico_Javascript/js/variables.js
@@ -0,0 +1,19 @@
+//Variables
+
+//Declarar
+let nombre;
+
+//Inicializar
+nombre = "Julio";
+
+
+let elementos = ["Celular", "Computadora"];
+typeof elementos;
+
+//Variable tipo objeto
+let persona = {
+    nombre: "Diego",
+    edad: 20
+}
+
+console.log(persona);
