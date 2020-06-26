8b43c4b58a63873d114137c804ece72dd009e3a2
Julio Gonzalez <1630368@upv.edu.mx>
2020-06-26 10:54:55 -0500

    Clase 7: Scopes

--- Curso_Basico_Javascript/js/funciones.js
+++ Curso_Basico_Javascript/js/funciones.js
@@ -1,18 +1,69 @@
 //Funciones declarativas
 
-function miFuncion()
+function miFuncion1()
 {
     return 3;
 }
 
-miFuncion();
+//miFuncion();
 
+
+let res;
 
 //Funciones Expresivas o anonimas
 let miFuncion = function(a,b){
 
-    return a + b;
+    //return a + b;
+   
+    res = a + b;
+
+    console.log(res);
 }
     
 
 miFuncion(5,5);
+
+function saludarEstudiante(estudiante)
+{
+    console.log(`Hola: ${estudiante} ` );
+}
+
+
+saludarEstudiante('Julio');
+
+
+//Funcion con palabra reservada return 
+function sumar(a,b)
+{
+    let resultado = a + b;
+    return resultado;
+}
+
+sumar(1,2);
+
+//
+let persona = {
+
+    nombre: "Julio",
+    edad: 22,
+}
+
+function presentarPersona(args)
+{
+    return `Hola me llamo ${args.nombre} y tengo ${args.edad}`;
+}
+
+presentarPersona(persona);
+
+
+//Funcion dentro de otra funcion 
+
+function a(x)
+{
+    function b(y)
+    {
+        return x + y;
+    }
+
+    return b;
+}
